3a0a398b8dde4dc2118985809e2fd4dd1d7cdfab
James Andariese <james.andariese@locationlabs.com>
2014-07-08 01:16:23 -0700

    initial import

---
+++ README.md
@@ -0,0 +1,3 @@
+A copytruncate tool for when logrotate has failed you.
+
+Gzips during rotation and tries to limit the window of fail as much as possible.
---
+++ copytruncate.go
@@ -0,0 +1,103 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"compress/gzip"
+	"os"
+	"flag"
+	"time"
+)
+
+var format = flag.String("format", "2006-02-01", "Time format")
+
+func removeUnderlyingFile(name string, file *os.File) {
+	fi, err := file.Stat()
+	if err != nil {
+		fmt.Fprintln(os.Stderr, "couldn't stat", name, err)
+		return
+	}
+	nfi, err := os.Stat(name)
+	if err != nil {
+		fmt.Fprintln(os.Stderr, "couldn't delete", name, err)
+		return
+	}
+	if os.SameFile(fi, nfi) {
+		os.Remove(name)
+	}
+}
+
+func copyTruncate(nameIn, nameOut string) error {
+	// copy with gzip and truncate a file.
+	// filename may be relative or absolute.
+	// this will cd to the directory with the filec.
+	out, err := os.Create(nameOut)
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		return err
+	}
+	in, err := os.OpenFile(nameIn, os.O_RDWR, os.ModePerm)
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		return err
+	}
+	defer out.Close()
+	defer in.Close()
+	defer func() {
+		// if the function fails, let's delete the file we were creating to save space.  OKAY!
+		if r := recover(); r != nil {
+			removeUnderlyingFile(nameOut, out)
+		}
+	}()
+
+	gzout := gzip.NewWriter(out)
+	defer gzout.Close()
+
+	// Don't truncate until we've got 10 empty reads in a row
+	for i := 0; i < 1000000; i++ {
+		written, err := io.Copy(gzout, in)
+		if written > 0 {
+			i = 0
+			fmt.Print(".")
+		}
+		if err != nil {
+			fmt.Fprintln(os.Stderr, err)
+			panic(err)
+		}
+	}
+	err = in.Truncate(0)
+	if err != nil {
+		fmt.Fprintln(os.Stderr, "Couldn't truncate file", err)
+		panic(err)
+	}
+	in.Sync()
+	fmt.Println("Done")
+	return nil
+}
+
+func fileExists(name string) bool {
+	_, err := os.Stat(name)
+	if os.IsNotExist(err) {
+		return false
+	}
+	// at this point, if it didn't exist, we'd know.
+	return true
+}
+
+func mainOnce(name string) {
+	now := time.Now().Format(*format)
+	outname := fmt.Sprintf("%s.%s.gz", name, now)
+	n := 0
+	for fileExists(outname) {
+		n += 1
+		outname = fmt.Sprintf("%s.%s.%d.gz", name, now, n)
+	}
+	copyTruncate(name, outname)
+}
+
+func main() {
+	flag.Parse()
+	for _, arg := range flag.Args() {
+		mainOnce(arg)
+	}
+}
